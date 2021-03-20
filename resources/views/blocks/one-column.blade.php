1c06d66c38aa04b8fdfbadb4b806509a119ff02a
denrahydnas <sandylin34@gmail.com>
2021-03-19 21:07:22 -0400

    one-column blade and basic variables and typography

--- resources/views/blocks/one-column.blade.php
+++ resources/views/blocks/one-column.blade.php
@@ -1,5 +1,10 @@
-<div class="block one-column-block">
-    <h2 class="text-center">This file is the One Column block.</h2>
-    <p class="text-center mb-0">File Located: /resources/views/blocks/one-column.blade.php</p>
-    <p class="text-center">Sass Located: /resources/blocks/_one-column.scss</p>
+<div class="block one-column-block content">
+    <h2 class="text-center">This is a headline</h2>
+    <hr style="border: none;">
+    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Purus semper eget duis at tellus at urna condimentum. Egestas maecenas pharetra convallis posuere morbi leo. 
+        Mi quis hendrerit dolor magna eget. Nisi est sit amet facilisis magna etiam. Donec et odio pellentesque diam volutpat. 
+        Non consectetur a erat nam. Diam donec adipiscing tristique risus nec feugiat. Eget gravida cum sociis natoque penatibus et magnis. 
+        Non diam phasellus vestibulum lorem sed. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. 
+        Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Urna et pharetra pharetra massa. </p>
 </div>
