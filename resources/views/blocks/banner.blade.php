f2ac13e21e0646f82b0136279106857961261ab1
denrahydnas <sandylin34@gmail.com>
2021-03-20 18:55:02 -0400

    added @extend

--- resources/views/blocks/banner.blade.php
+++ resources/views/blocks/banner.blade.php
@@ -1,4 +1,4 @@
 <div class="block banner-block">
-    <h2 class="banner">This is a</h2>
-    <h1 class="banner">Webpage<h1>
+    <h2 class ="ban-text">This is a</h2>
+    <h1 class="ban-text">Webpage<h1>
 </div>
