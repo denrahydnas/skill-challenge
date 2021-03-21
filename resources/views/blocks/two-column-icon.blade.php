ee00d12b52abd63df23fc811245e965ebb12811d
denrahydnas <sandylin34@gmail.com>
2021-03-21 18:05:23 -0400

    fixed order swap

--- resources/views/blocks/two-column-icon.blade.php
+++ resources/views/blocks/two-column-icon.blade.php
@@ -2,7 +2,7 @@
     <h3 class="text-center">This is a second headline</h3>
     <hr style="border: none;">
     <div class="card-group text-block">
-        <div class="card col-left mt-3 .order-sm-12">
+        <div class="card col-right mt-3 order-sm-12">
             <span class= "img2"> </span>   
             <div class="card-block">
 <!--                <img class="card-img-top" src="public/images/icon_lockup.png" alt="icons"> -->
@@ -12,7 +12,7 @@
                 Non consectetur a erat nam. Diam donec adipiscing tristique risus nec feugiat. Eget gravida cum sociis natoque penatibus et magnis. </p>
             </div>
         </div>
-        <div class="card col-right mt-3 .order-sm-1">
+        <div class="card col-left mt-3 order-sm-1">
             <span class= "img1"> </span>
 <!--            <img class="card-img-top card-right mt-3" src="../../../public/images/girl-in-kayak-in-mountain-lake.jpg" alt="girl in kayak"> -->
         </div>
