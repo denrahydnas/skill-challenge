610b24d73215322c40c7e79533254dd535e45108
denrahydnas <sandylin34@gmail.com>
2021-03-21 12:43:26 -0400

    img issue workaround

--- resources/views/blocks/two-column.blade.php
+++ resources/views/blocks/two-column.blade.php
@@ -4,7 +4,7 @@
     <div class="card-group text-block">
         <div class="card">
             <div class="card-block">
-            <p class="card-text card-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            <p class="card-text col-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Purus semper eget duis at tellus at urna condimentum. Egestas maecenas pharetra convallis posuere morbi leo. 
                 Mi quis hendrerit dolor magna eget. Nisi est sit amet facilisis magna etiam. Donec et odio pellentesque diam volutpat. 
                 Non consectetur a erat nam. Diam donec adipiscing tristique risus nec feugiat. Eget gravida cum sociis natoque penatibus et magnis. 
@@ -13,7 +13,9 @@
             </div>
         </div>
         <div class="card">
-            <img class="card-img-top card-right mt-3" src="../../../public/images/girl-in-kayak-in-mountain-lake.jpg" alt="girl in kayak">
+            <span class= "img3 col-right mt-3"> </span>
+<!--            <img class="card-img-top card-right mt-3" src="../../../public/images/girl-in-kayak-in-mountain-lake.jpg" alt="girl in kayak"> -->
         </div>
     </div>
 </div>
+
