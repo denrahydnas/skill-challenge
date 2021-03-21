bc9a4ff6c2acb6d20013c9adbf6d31542288c698
denrahydnas <sandylin34@gmail.com>
2021-03-21 13:25:06 -0400

    img issue workaround implemented

--- resources/views/blocks/two-column-icon.blade.php
+++ resources/views/blocks/two-column-icon.blade.php
@@ -1,24 +1,23 @@
 <div class="block two-column-icon-block">
     <h3 class="text-center">This is a second headline</h3>
     <hr style="border: none;">
-    <div class="row text-block">
-        <div class="col-12 col-md-6">
-            <img class="card-left mt-3" src="../../../public/images/boys-on-hill.jpg" alt="girl in kayak">
-        </div>
-        <div class="col-12 col-md-6">
-            <div class="row card-right mt-3">
-                <div class="col-12 justify-content-center">
-                    <span class="img2">
-                </div>
-                <div class="col-12">            
-                    <p class="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Purus semper eget duis at tellus at urna condimentum. Egestas maecenas pharetra convallis posuere morbi leo. 
-                    Mi quis hendrerit dolor magna eget. Nisi est sit amet facilisis magna etiam. Donec et odio pellentesque diam volutpat. 
-                    Non consectetur a erat nam. Diam donec adipiscing tristique risus nec feugiat. Eget gravida cum sociis natoque penatibus et magnis. 
-                    Non diam phasellus vestibulum lorem sed. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. 
-                    Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Urna et pharetra pharetra massa. </p></div>
-                </div>
+    <div class="card-group text-block">
+        <div class="card col-left mt-3 .order-sm-12">
+            <span class= "img2"> </span>   
+            <div class="card-block">
+<!--                <img class="card-img-top" src="public/images/icon_lockup.png" alt="icons"> -->
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Purus semper eget duis at tellus at urna condimentum. Egestas maecenas pharetra convallis posuere morbi leo. 
+                Mi quis hendrerit dolor magna eget. Nisi est sit amet facilisis magna etiam. Donec et odio pellentesque diam volutpat. 
+                Non consectetur a erat nam. Diam donec adipiscing tristique risus nec feugiat. Eget gravida cum sociis natoque penatibus et magnis. </p>
             </div>
         </div>
+        <div class="card col-right mt-3 .order-sm-1">
+            <span class= "img1"> </span>
+<!--            <img class="card-img-top card-right mt-3" src="../../../public/images/girl-in-kayak-in-mountain-lake.jpg" alt="girl in kayak"> -->
+        </div>
     </div>
+</div>
+
+
 </div>
