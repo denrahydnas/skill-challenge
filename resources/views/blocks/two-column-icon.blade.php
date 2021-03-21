0dc4480b2694cf75dd69e63d46c05cf0a268d30a
denrahydnas <sandylin34@gmail.com>
2021-03-20 21:14:21 -0400

    start on two column with icon layout, continuing img issues

--- resources/views/blocks/two-column-icon.blade.php
+++ resources/views/blocks/two-column-icon.blade.php
@@ -1,5 +1,30 @@
 <div class="block two-column-icon-block">
-    <h2 class="text-center">This file is the Two Column Icon block.</h2>
-    <p class="text-center mb-0">File Located: /resources/views/blocks/two-column-icon.blade.php</p>
-    <p class="text-center">Sass Located: /resources/blocks/_two-column-icon.scss</p>
+    <h3 class="text-center">This is a second headline</h3>
+    <hr style="border: none;">
+    <div class="row text-block">
+        <div class="col-12 col-md-6">
+            <img class="card-left mt-3" src="../../../public/images/girl-in-kayak-in-mountain-lake.jpg" alt="girl in kayak">
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="row card-right mt-3">
+                <div class="col-4 justify-content-center">
+                    <img class="icon" src="../../../public/images/globe.png" alt="globe">"
+                </div>
+                <div class="col-4 justify-content-center">
+                    <img class="icon" src="../../../public/images/train.png" alt="train">"
+                </div>
+                <div class="col-4 justify-content-center">
+                    <img class="icon" src="../../../public/images/plane.png" alt="plane">"
+                </div>
+                <div class="col-12">            
+                    <p class="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Purus semper eget duis at tellus at urna condimentum. Egestas maecenas pharetra convallis posuere morbi leo. 
+                    Mi quis hendrerit dolor magna eget. Nisi est sit amet facilisis magna etiam. Donec et odio pellentesque diam volutpat. 
+                    Non consectetur a erat nam. Diam donec adipiscing tristique risus nec feugiat. Eget gravida cum sociis natoque penatibus et magnis. 
+                    Non diam phasellus vestibulum lorem sed. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. 
+                    Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Urna et pharetra pharetra massa. </p></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
