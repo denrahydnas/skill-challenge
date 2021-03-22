13c0077271ce68c1434c854875d42651ec67ddcb
denrahydnas <sandylin34@gmail.com>
2021-03-22 19:27:08 -0400

    yellow hamburger

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -24,7 +24,7 @@
         </li>
       </ul>
     </div> 
-    <button class="navbar-toggler toggle" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <button class=" custom-toggler navbar-toggler toggle" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 </nav>
