9395195b248e6999b8a5faac91cdae5f0c0320e3
denrahydnas <sandylin34@gmail.com>
2021-03-22 18:35:24 -0400

    figuring out sliding panel

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,2 +1,8 @@
 console.log('Welcome to the Skill Challenge')
 
+const button = document.querySelector('.toggle');
+const pane = document.querySelector('.pane');
+
+button.addEventListener('click', () => {
+  pane.classList.toggle('open');
+});
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-sm navbar-dark">
+<!-- <nav class="navbar navbar-expand-sm navbar-dark">
     <a class="navbar-brand" href="#">
       <h5>This is a</h5>
       <h4>Logo</h4>
@@ -30,5 +30,60 @@
         </li>
       </ul>
     </div>  
-</nav>   
+</nav> -->  
+<nav class="navbar navbar-expand-sm navbar-dark">
+    <a class="navbar-brand" href="#">
+      <h5>This is a</h5>
+      <h4>Logo</h4>
+    </a>
+    <div class="collapse navbar-collapse" id="navbar-content">
+      <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+            <a class="nav-link" href="#">
+            <h6>Locations</h6>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+            <h6>Travel</h6>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+              <h6>Booking</h6>
+            </a>
+        </li>
+      </ul>
+    </div> 
+    <button class="navbar-toggler toggle" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</nav>
+
+<div class="pane">
+<ul class="hidden-pane ml-auto">
+      <li class="nav-item">
+            <a class="nav-link" href="#">
+            <h6>Locations</h6>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+            <h6>Travel</h6>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+              <h6>Booking</h6>
+            </a>
+        </li>
+      </ul>
+  </div>
+  <!--<button class="toggle">Toggle
+  </button>-->
+
 
