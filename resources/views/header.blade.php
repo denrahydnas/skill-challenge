0ae2fd32e0bb764a1dd645301643bc9bdc9dbb4c
denrahydnas <sandylin34@gmail.com>
2021-03-22 19:17:43 -0400

    sliding nav work

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,36 +1,3 @@
-<!-- <nav class="navbar navbar-expand-sm navbar-dark">
-    <a class="navbar-brand" href="#">
-      <h5>This is a</h5>
-      <h4>Logo</h4>
-    </a>
-
-    <input type="checkbox" id="new-navbar-toggler" class="d-none" />
-    <label for="new-navbar-toggler" class="navbar-toggler ml-auto custom-toggler" data-toggle="collapse" data-target="#new-navbar-content" aria-controls="lua-navbar-content" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </label> 
-        
-    <div class="collapse navbar-collapse" id="navbar-content">
-      <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-            <a class="nav-link" href="#">
-            <h6>Locations</h6>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-            <h6>Travel</h6>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-              <h6>Booking</h6>
-            </a>
-        </li>
-      </ul>
-    </div>  
-</nav> -->  
 <nav class="navbar navbar-expand-sm navbar-dark">
     <a class="navbar-brand" href="#">
       <h5>This is a</h5>
@@ -62,28 +29,17 @@
   </button>
 </nav>
 
-<div class="pane">
-<ul class="hidden-pane ml-auto">
-      <li class="nav-item">
-            <a class="nav-link" href="#">
-            <h6>Locations</h6>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-            <h6>Travel</h6>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-              <h6>Booking</h6>
-            </a>
-        </li>
-      </ul>
+  <div class="pane ml-auto">
+    <a class="hidden-link" href="#">
+      <h6>Locations</h6>
+    </a>
+    <a class="hidden-link" href="#">
+      <h6>Travel</h6>
+    </a>
+    <a class="hidden-link" href="#">
+      <h6>Booking</h6>
+    </a>
   </div>
-  <!--<button class="toggle">Toggle
-  </button>-->
+
 
 
