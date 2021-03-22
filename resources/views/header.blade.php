9446e693c3eaf60e9ea7ce937b63475a08e2a7eb
denrahydnas <sandylin34@gmail.com>
2021-03-21 20:16:04 -0400

    working on nav transitions

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,43 @@
-<nav class="text-center py-3">
-    <h2>This is the Nav</h2>
-    <p class="mb-0">File Located:  /resources/views/header.blade.php</p>
-    <p class="text-center">Sass Located: /resources/_nav.scss</p>
-</nav>
+<nav class="navbar navbar-expand-xl navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <h5>This is a</h5>
+      <h4>Logo</h4>
+    </a>
+
+    <!--
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#lua-navbar-content" aria-controls="lua-navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    -->
+
+    <input type="checkbox" id="new-navbar-toggler" class="d-none" />
+
+    <label for="new-navbar-toggler" class="navbar-toggler" data-toggle="collapse" data-target="#new-navbar-content" aria-controls="lua-navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </label> 
+
+    <div class="collapse navbar-collapse" id="lua-navbar-content">
+      <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+            <a class="nav-link" href="#">
+            <h6>Locations</h6>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+            <h6>Travel</h6>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+              <h6>Booking</h6>
+            </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>   
+
